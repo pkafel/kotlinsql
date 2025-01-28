e75e420d72af462ec9551dbf0479b2f162964bdc
pkafel <3944981+pkafel@users.noreply.github.com>
2025-01-28 21:17:22 +0100

    Initial version of AST structures

---
+++ src/main/kotlin/com/piotrkafel/kotlinsql/Ast.kt
@@ -0,0 +1,36 @@
+package com.piotrkafel.kotlinsql
+
+sealed interface Statement {
+
+    data class CreateTableStatement(
+        val name: String,
+        val columns: List<ColumnDefinition>
+    ) : Statement
+
+    data class InsertStatement(
+        val tableName: String,
+        val values: List<Literal>
+    ) : Statement
+
+    data class SelectStatement(
+        val columns: List<String>,
+        val tableName: String
+    ) : Statement
+
+}
+
+data class ColumnDefinition(
+    val name: String,
+    val type: ColumnType
+)
+
+enum class ColumnType {
+    INT, TEXT
+}
+
+sealed interface Literal {
+
+    data class IntLiteral(val value: Int) : Literal
+
+    data class StringLiteral(val value: String) : Literal
+}
